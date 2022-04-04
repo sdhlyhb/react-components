1c41ebe27759ded6fd82416c9acc9938bfc0bb99
Danting Huang <serena.huangdanting@gmail.com>
2022-04-03 23:30:06 -0400

    Complete

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItems.map((item) =>
+    <GroceryListItem item = {item}/>
+    )}
+  </ul>
+
+);
+
+
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //hover state
+    this.state = {
+    hover: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+
+  };
+}
+
+// var GroceryListItem = () => (
+//   <div>
+//     <h2>My Grocery List Items</h2>
+//     <GroceryList groceryItems={['kale', 'cucumber', 'milk']} />;
+
+//   </div>
+
+// );
+
+ReactDOM.render(<GroceryList groceryItems={['kale', 'cucumber', 'milk']}  />, document.getElementById("app"));
